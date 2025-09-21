23c86bcb1d2033617f4f9625679d2f4c1be6dd5b
arymandeshwal <deshwal.aryman@gmail.com>
2025-09-21 23:43:50 +0200

    dekhte hai ab

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      {/* --- ADD THIS HEAD SECTION --- */}
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#818cf8" />
+      </head>
       <body className={inter.className} suppressHydrationWarning={true}>
         {children}
       </body>
