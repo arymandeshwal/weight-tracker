51c0ce78a797a9bd4a3ecfa219f3d2923490a0bb
arymandeshwal <deshwal.aryman@gmail.com>
2025-09-21 19:39:25 +0200

    Feat: Implement PWA and 'Remember Me' functionality

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+import nextPwa from "next-pwa";
+
+const withPWA = nextPwa({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
